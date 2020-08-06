62bfb0c1ccede040bdc231fc63afd9c67aede803
baloghdominik <balogh0dominik@gmail.com>
2020-08-06 07:11:09 +0200

    fix

--- resources/views/panels/sidebar.blade.php
+++ resources/views/panels/sidebar.blade.php
@@ -4,8 +4,7 @@
 <div class="main-menu {{ $configData['menuType'] }} {{($configData['theme'] === 'light') ? "menu-light" : "menu-dark"}} menu-accordion menu-shadow" data-scroll-to-active="true">
     <div class="navbar-header">
         <ul class="nav navbar-nav flex-row">
-            <li class="nav-item mr-auto"><a class="navbar-brand" href="home">
-                    <div class="brand-logo"></div>
+            <li class="nav-item mr-auto ml-auto"><a class="navbar-brand" href="home">
                     <h2 class="brand-text mb-0 with-logo">With</h2>
                 </a></li>
             <li class="nav-item nav-toggle"><a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"><i class="feather icon-x d-block d-xl-none font-medium-4 primary toggle-icon"></i><i class="toggle-icon feather icon-disc font-medium-4 d-none d-xl-block primary collapse-toggle-icon" data-ticon="icon-disc"></i></a></li>
