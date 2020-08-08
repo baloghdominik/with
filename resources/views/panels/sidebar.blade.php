4b12eadc90962616d9271e58633c440709c72f90
baloghdominik <balogh0dominik@gmail.com>
2020-08-09 01:13:19 +0200

    changed with logo to withPOS (point of sale)

--- resources/views/panels/sidebar.blade.php
+++ resources/views/panels/sidebar.blade.php
@@ -5,7 +5,7 @@
     <div class="navbar-header">
         <ul class="nav navbar-nav flex-row">
             <li class="nav-item mr-auto ml-auto"><a class="navbar-brand" href="{{ url('home') }}">
-                    <h2 class="brand-text mb-0 with-logo">With</h2>
+                    <h2 class="brand-text mb-0 with-logo">With<span>POS</span></h2>
                 </a></li>
             <li class="nav-item nav-toggle"><a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"><i class="feather icon-x d-block d-xl-none font-medium-4 primary toggle-icon"></i><i class="toggle-icon feather icon-disc font-medium-4 d-none d-xl-block primary collapse-toggle-icon" data-ticon="icon-disc"></i></a></li>
         </ul>
